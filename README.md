5070df7b099a74ea0843147424608b4b71298d47
antonielbordin <antonielbordin@hotmail.com>
2023-11-14 21:47:37 -0300

    Start documentation

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# BirdieUI
+# Documentation BirdieUI
+
 Birdie UI is a library of UI components that you can use to start any web project
+
+## Recommended for using
+
+
+ 
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/birdie-ui/',
   plugins: [svelte()],
-  build: { outDir: "../docs" }
+  build: { outDir: "./docs" }
 })
