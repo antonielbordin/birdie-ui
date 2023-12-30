0a6c68ad8bf6fd4aa5c9c2a14a0558247960b510
antonielbordin <antonielbordin@hotmail.com>
2023-12-29 23:01:30 -0300

    Changing the component for testing

---
+++ esbuild.js
@@ -0,0 +1,41 @@
+const esbuild = require('esbuild');
+const sveltePlugin = require('esbuild-svelte');
+const pkg = require('./package.json');
+
+(async ()=>{
+    await esbuild.build({
+        entryPoints: ['./svelte.js'],
+        bundle: true,
+        outfile: pkg.module,
+        format: 'esm',
+        minify: true,
+        external: [
+            'svelte',
+            'svelte/*'
+        ],
+        plugins: [sveltePlugin({
+          compileOptions:{
+            dev: false,
+            css: true
+		      }
+        })]
+    });
+
+    await esbuild.build({
+        entryPoints: ['./svelte.js'],
+        bundle: true,
+        outfile: pkg.main,
+        format: 'cjs',
+        minify: true,
+        external: [
+            'svelte',
+            'svelte/*'
+        ],
+        plugins: [sveltePlugin({
+          compileOptions:{
+            dev: false,
+            css: true
+		      }
+        })]
+    });
+})()
